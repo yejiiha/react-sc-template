49830128265af4ea473a4372f27e5e9af9b62ec4
yejiiha <1126yezy@gmail.com>
2024-10-02 01:27:00 +0900

    styled-components 세팅 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+/* eslint-disable import/order */
+
+import { GlobalStyle } from '@/styles/global';
+import theme from '@/styles/theme';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
